083845f7cff5a9537d69e9b58236bcaef2fac22b
themilnera <amilner32@gmail.com>
2025-11-12 14:42:51 -0700

    Finished the account dashboard for now.

--- src/app/api/user/[id]/route.ts
+++ src/app/api/user/[id]/route.ts
@@ -12,11 +12,11 @@ export async function GET(
   try {
     const { id } = await params;
 
-    const result = await pool.query(
-      "SELECT * FROM users WHERE user_id = $1", [id]
-    );
+    const result = await pool.query("SELECT * FROM users WHERE user_id = $1", [
+      id,
+    ]);
 
-    return NextResponse.json( result );
+    return NextResponse.json(result);
   } catch (error) {
     console.error("Database error:", error);
     return NextResponse.json(
@@ -26,3 +26,25 @@ export async function GET(
   }
 }
 
+export async function PUT(
+  request: NextRequest,
+  { params }: { params: { id: string } }
+) {
+  try {
+    const { id } = await params;
+    const body = await request.json();
+    const { seller_name, seller_image_url } = body;
+    const result = await pool.query(
+      `
+      UPDATE users
+      SET seller_name = $2, seller_image_url = $3
+      WHERE user_id = $1
+      RETURNING *
+      `,
+      [id, seller_name, seller_image_url]
+    );
+    return NextResponse.json(result);
+  } catch (error) {
+    console.error("Failed to update user database object: ", error);
+  }
+}
--- src/components/account-client.tsx
+++ src/components/account-client.tsx
@@ -18,6 +18,14 @@ export default function Account({
   const [opened, { open, close }] = useDisclosure(false);
   const [profileimage, setProfileImage] = useState<string>();
   const [gameDay, setGameDay] = useState();
+  const [cash, setCash] = useState();
+
+  const [updateDisabled, setUpdateDisabled] = useState(false);
+
+  //These are not in the db yet, add them
+  const [itemsSold, setItemsSold] = useState(0);
+  const [itemsPurchased, setItemsPurchased] = useState(0);
+  
 
   const fetchUserDbObject = async () => {
     try {
@@ -27,13 +35,27 @@ export default function Account({
         router.push("/new-day");
       }
       const userObject = result.data.rows[0];
-      console.log(userObject)
+      console.log(userObject);
       setSellerName(userObject.seller_name);
       setProfileImage(userObject.seller_image_url);
       setGameDay(userObject.current_day);
+      setCash(userObject.money);
     } catch (error) {
       console.error("Failed to fetch userDbObject: ", error);
     }
+  };
+
+  const updateUserDbObject = async () => {
+    try {
+      setUpdateDisabled(true);
+      const result = await axios.put(`api/user/${user?.id}`, {
+        seller_name: sellerName,
+        seller_image_url: profileimage,
+      });
+    } catch (error) {
+      console.error("Failed to push user account updates to database: ", error);
+    }
+    setUpdateDisabled(false);
   };
 
   useEffect(() => {
@@ -94,7 +116,7 @@ export default function Account({
           <span className="mt-10 font-semibold text-2xl">
             Account Dashboard
           </span>
-          <div className="border-2  w-[70%] h-150 rounded-2xl mt-10 border-gray-700 bg-gray-400 flex flex-col">
+          <div className="border-2  w-[70%] h-[100%] rounded-2xl mt-10 border-gray-700 bg-gray-400 flex flex-col">
             <div className="flex md:flex-row flex-col">
               <div className="m-10 flex flex-1 flex-col gap-5">
                 <span className="flex items-center">
@@ -110,6 +132,7 @@ export default function Account({
                     }}
                   ></TextInput>
                 </span>
+
                 <span className="flex items-center gap-5 font-semibold">
                   Change Profile Image:{" "}
                   <Image
@@ -121,27 +144,59 @@ export default function Account({
                     onClick={open}
                   ></Image>{" "}
                 </span>
-                  <Button onClick={()=>{
-                    auth.signOut();
-                  }} className="bg-blue-700!" radius={'md'}>Log Out</Button>
+
+                <div className="flex justify-around mt-10 gap-3">
+                  {!updateDisabled ? (
+                    <Button
+                      onClick={() => {
+                        updateUserDbObject();
+                      }}
+                      className="bg-green-600! flex-2"
+                      radius={"md"}
+                    >
+                      Update
+                    </Button>
+                  ) : (
+                    <div className="loader"></div>
+                  )}
+
+                  <Button
+                    onClick={() => {
+                      auth.signOut();
+                    }}
+                    className="bg-blue-700! flex-1"
+                    radius={"md"}
+                  >
+                    Log Out
+                  </Button>
+                </div>
               </div>
               <div className="font-semibold m-5 h-[100%] p-5 flex flex-3 flex-col items-center gap-5 border-2 rounded-2xl border-gray-500 bg- bg-gray-500">
                 <span className="text-2xl text-gray-200">
                   Current Game Stats
                 </span>
-                <span className="mr-auto w-[100%] border-b-1 bg-gray-400 p-2 rounded-md">Game Day: </span>
-                <span className="mr-auto w-[100%] border-b-1 bg-gray-400 p-2 rounded-md">Current Cash: </span>
-                <span className="mr-auto w-[100%] border-b-1 bg-gray-400 p-2 rounded-md">Items Sold: </span>
-                <span className="mr-auto w-[100%] border-b-1 bg-gray-400 p-2 rounded-md">Items Purchased: </span>
-                <span className="mr-auto w-[100%] border-b-1 bg-gray-400 p-2 rounded-md">Seller Rating: </span>
+                <span className="w-[100%] border-b-1 bg-gray-400 p-2 rounded-md flex items-center justify-between">
+                  <span>Game Day:</span> <span className="text-xl mr-3">{gameDay}</span>
+                </span>
+                <span className="w-[100%] border-b-1 bg-gray-400 p-2 rounded-md flex items-center justify-between">
+                  <span>Current Cash:</span> <span className="text-xl mr-3">${cash}</span>
+                </span>
+                <span className="w-[100%] border-b-1 bg-gray-400 p-2 rounded-md flex items-center justify-between">
+                  <span>Items Sold:</span> <span className="text-xl mr-3">{itemsSold}</span>
+                </span>
+                <span className="w-[100%] border-b-1 bg-gray-400 p-2 rounded-md flex items-center justify-between">
+                  <span>Items Purchased:</span> <span className="text-xl mr-3">{itemsPurchased}</span>
+                </span>
               </div>
             </div>
             <div className="mt-auto mb-5 self-center flex flex-col items-center gap-5">
-                <span className="text-xl font-bold underline underline-offset-7">Danger Zone</span>
-                <div className="self-center flex gap-5 mb-5">
-                    <Button className="bg-violet-900!">Reset All Stats</Button>
-                    <Button className=" bg-red-900!">Delete Account</Button>
-                </div>
+              <span className="text-xl font-bold underline underline-offset-7">
+                Danger Zone
+              </span>
+              <div className="self-center flex gap-5 mb-5">
+                <Button className="bg-violet-900!">Reset All Stats</Button>
+                <Button className=" bg-red-900!">Delete Account</Button>
+              </div>
             </div>
           </div>
         </div>
